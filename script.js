4284103825232f2e5456d40db8c4ea8693a8af5f
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-07-24 17:11:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 for(let a in sampleObject){
-              if(sampleObject[a]===key){
+              if(sampleObject[a]==key){
                 return true;
               }
               else{
