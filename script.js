de7c14f8e5cb1fcdde127f0f5b64647240093767
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-08-11 22:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
-function hasKey(key) {
-  //   write your code here
-for(let a in sampleObject){
-              if(sampleObject[a]==key){
-                
-				  console.log(true);
-				  return true;
-              }
-              else{
-				  console.log(false);
-                return false;
-              }
-            }
-
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+  function hasKey(key) {
+    //   write your code here
+    for (let i in sampleObject){
+      if(i == key){
+        return true
+      }
+      return false
+    }
+  }
+  
+  // Do not change the code below
+  const key = prompt("Enter Key.");
+  alert(hasKey(key));
